301adcafdef1d70bdd007de0fcc5ca1b7f1685e7
bashair yaqoob <bashairyaqoob@gmail.com>
2024-09-04 20:51:35 +0500

    Update slope.c

--- PF LAB/slope.c
+++ PF LAB/slope.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main () {
-int A, B, c, m ;
+int A, B, c;
+double m ;
 printf("Enter x co-ordinate: ");
 scanf("%d", &A);
 printf("\nEnter y co-ordinate:");
 scanf("%d", &B);
 printf("\nEnter y-intercept:");
 scanf("%d", &c);
-m = ((B - c) / A) ;
-printf("The slope is: %d", m);
+m = (double)(B - c)/A ;
+printf("The slope is: %.3f\n", m);
     return 0;
 }
