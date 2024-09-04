30ffcaac344e10cd17d079544e2aa6b499420a62
bashair yaqoob <bashairyaqoob@gmail.com>
2024-09-04 15:12:56 +0500

    Add files via upload

---
+++ PF LAB/input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main () {
+int num1, num2, c, d;
+printf("enter number 1: ");
+scanf("%d", &num1);
+
+printf("enter number 2: ");
+scanf("%d", &num2);
+c = num2;
+d = num1;
+printf ("output is = %d , %d",c, d);
+return 0;
+}
