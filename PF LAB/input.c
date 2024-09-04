fbdb72cfe1aab1c802f5ab351f65e66bfe25d17e
bashair yaqoob <bashairyaqoob@gmail.com>
2024-09-04 15:19:46 +0500

    Update input.c

--- PF LAB/input.c
+++ PF LAB/input.c
@@ -1,3 +1,9 @@
+Answer 1️⃣: The int data type in C typically has a range of -2,147,483,648 to 2,147,483,647.
+The value 3000000000 exceeds this range . The printed output is the highest value that int can take.
+
+
+Answer 2️⃣:
+  
 #include <stdio.h>
 int main () {
 int num1, num2, c, d;
@@ -11,3 +17,7 @@ d = num1;
 printf ("output is = %d , %d",c, d);
 return 0;
 }
+
+
+Answer 3️⃣:
+
